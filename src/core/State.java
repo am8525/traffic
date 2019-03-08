cc0279e5cf933b1f6d6740ec7052428251cabe8c
Zerg <amcfarlane52@gmail.com>
2019-03-08 00:10:48 -0500

    Removed a state, added a state
    Removed 'AbortingLaneChange', as it is unnecessary; we can go
    straight into 'Fwd_MaxSpeed'.
    
    Added 'CloseToGoal'; when the smart car is within a threshold of
    the end of the screen, we enter this state, which tells us to
    get into the middle lane as fast as possible.

--- src/core/State.java
+++ src/core/State.java
@@ -2,16 +2,17 @@ package core;
 
 /**
  * @author ajm
- *
+ * These are the possible states 
+ * our smart car may be in.
  */
 public enum State {
 	
-	Fwd_MaxSpeed,
+	Fwd_MaxSpeed, //in this state, we are going straight away in the current lane, max speed.
 	
-	Braking,
+	Braking, //a car is ahead in our AOE; looking to make a lane change, if possible.
 	
-	ChangingLanes,
+	ChangingLanes, //in the midst of a lane change; continue changing if safe still, or go back to current if unsafe.
 	
-	AbortingLaneChange
+	CloseToGoal //enter this state when within a certain threshold of the end of the screen; must get into middle lane.
 	
 }
